4a8b98a9c9559957802fa559fb7f01ac4a74dd17
Ahsan Asghar <ahsanasghar10183@gmail.com>
2024-09-24 12:49:52 +0500

    Added Jquery Logic

--- Js/index.js
+++ Js/index.js
@@ -1,14 +1,30 @@
-const btn_on= document.querySelectorAll('.turn-off')[0];
-const btn_off= document.querySelectorAll('.turn-on')[0];
-const bulb_image = document.getElementById("light_image");
-// console.log(bulb_image);
-const turn_light_on = () => {
-    bulb_image.src="./Images/lightbulb-png-833.png"
-}
-const turn_light_off = () => {
-    bulb_image.src="./Images/lightbulb-png-830.png"
-}
+// const btn_on= document.querySelectorAll('.turn-off')[0];
+// const btn_off= document.querySelectorAll('.turn-on')[0];
+// const bulb_image = document.getElementById("light_image");
+// // console.log(bulb_image);
+// const turn_light_on = () => {
+//     bulb_image.src="./Images/lightbulb-png-833.png"
+// }
+// const turn_light_off = () => {
+//     bulb_image.src="./Images/lightbulb-png-830.png"
+// }
 
-btn_on.addEventListener('click', turn_light_on);
-btn_off.addEventListener ('click', turn_light_off );
+// btn_on.addEventListener('click', turn_light_on);
+// btn_off.addEventListener ('click', turn_light_off );
+$(document).ready(function(){
 
+    $('button.turn-off').click(function(){
+       console.log( $("#light_image").attr('src', './Images/lightbulb-png-833.png'));
+    
+        console.log('Button is clicked');
+    })
+    $('button.turn-on').click(function(){
+        console.log( $("#light_image").attr('src', './Images/lightbulb-png-830.png'));
+        console.log('Button is clicked');
+
+    })
+
+  
+  });
+
+   
